f83a60ac9d63a38c7c2f9543a9976c9ae243be1d
superSyd <37779021+superSyd@users.noreply.github.com>
2021-04-13 04:48:53 +1000

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,14 @@
 'use strict';
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DB, {
+  useNewURLParser: true, 
+  useUnifiedTopology: true
+});
+
+if(!mongoose.connection.readyState){
+  console.log("database error")
+}
 
 module.exports = function (app) {
 
@@ -46,23 +56,56 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
-      let id = req.body._id;
-      let issue_title = req.body.issue_title;
-      let issue_text = req.body.issue_text;
-      let created_on = req.body.created_on;
-      let updated_on = req.body.updated_on;
-      let created_by = req.body.created_by;
-      let assigned_to = req.body.assigned_to;
-      let open = req.body.open;
-      let status_text = req.body.status_text;
-
-      var issueList = {}
-
-     
-
+      let _id = req.query._id;
+      let issue_title = req.query.issue_title;
+      let issue_text = req.query.issue_text;
+      let created_on = req.query.created_on;
+      let updated_on = req.query.updated_on;
+      let created_by = req.query.created_by;
+      let assigned_to = req.query.assigned_to;
+      let open = req.query.open;
+      let status_text = req.query.status_text;
 
       if(project){
-        Issue.find({project: project, _id: id, issue_title: issue_title, issue_text: issue_text, created_on: created_on, updated_on: updated_on, created_by: created_by, assigned_to: assigned_to, open: open, status_text: status_text},(err,issues)=>{
+
+        const filter = {};
+
+        filter.project = project
+
+        if(_id){
+          filter._id = _id
+        }
+        if(issue_title){
+          filter.issue_title = issue_title
+        }
+        if(issue_text){
+          filter.issue_text = issue_text
+        }
+        if(created_on){
+          filter.created_on = created_on
+        }
+        if(updated_on){
+          filter.updated_on = updated_on
+        }
+        if(created_by){
+          filter.created_by = created_by
+        }
+        if(assigned_to){
+          filter.assigned_to = assigned_to
+        }
+
+        if(open != undefined){
+          filter.open = open
+
+        }
+
+        if(status_text){
+          filter.status_text = status_text
+        }
+
+
+        console.log(filter)
+        Issue.find(filter,(err,issues)=>{
           
           if(err){
             return res.json({
@@ -70,8 +113,9 @@ module.exports = function (app) {
             })
           }
 
-          var issueObject = issues.map(issues =>{
-            return { 
+          var issueObject = issues.map(issue =>{
+            
+              return { 
             "_id": issue._id,
             "issue_title": issue.issue_title,
             "issue_text": issue.issue_text,
@@ -82,6 +126,8 @@ module.exports = function (app) {
             "open": issue.open,
             "status_text": issue.status_text
               }
+
+            
       
             })
 
@@ -101,6 +147,7 @@ module.exports = function (app) {
     })
     
     .post(function (req, res){
+      
       let project = req.params.project;
       let issue_title = req.body.issue_title;
       let issue_text = req.body.issue_text;
@@ -113,14 +160,11 @@ module.exports = function (app) {
 
 
       if(project){
-        if(!issue_title){
-          return res.send('Please enter issue title')
-
-        } else if(!issue_text){
-          return res.send('Please enter issue text')
-
-        } else if(!created_by){
-          return res.send('Please enter creator')
+        if(!issue_title || !issue_text || !created_by){
+          
+          return res.json({
+            error: 'required field(s) missing'
+          })
           
         } else {
 
@@ -136,8 +180,9 @@ module.exports = function (app) {
         status_text: status_text
         });
 
+
         myIssue.save((err,issue)=>{
-        if(err){
+        if(err || issue === null){
             return res.json({
             "error": err
             })
@@ -168,65 +213,121 @@ module.exports = function (app) {
     })
     
     .put(function (req, res){
+
+          console.log(req.url)
+
+
       let project = req.params.project;
-      let id = req.body._id;
-      let issue_title = req.body.issue_title ? req.body.issue_title : "";
-      let issue_text = req.body.issue_text ? req.body.issue_text : "";
-      let updated_on = new Date().toISOString();
-      let created_by = req.body.created_by ?  req.body.created_by : "";
-      let assigned_to = req.body.assigned_to ? req.body.assigned_to : "" ;
+      let _id = req.body._id;
+      let issue_title = req.body.issue_title;
+      let issue_text = req.body.issue_text;
+      let created_by = req.body.created_by;
+      let assigned_to = req.body.assigned_to;
       let open = req.body.status_text;
-      let status_text = req.body.status_text ? req.body.status_text : "";
+      let status_text = req.body.status_text;
+
+      console.log(project,_id,issue_title,issue_text,created_by,assigned_to,open,status_text)
+
+      const updateFields = {};
+
+        
+        if(issue_title){
+          updateFields.issue_title = issue_title
+        }
+        if(issue_text){
+          updateFields.issue_text = issue_text
+        }
+        
+        if(created_by){
+          updateFields.created_by = created_by
+        }
+        if(assigned_to){
+          updateFields.assigned_to = assigned_to
+        }
+
+        if(status_text){
+          updateFields.status_text = status_text
+        }
 
 
       if(project){
-        if(!id){
-        return res.send('Please enter id')
+        if(_id == undefined || _id == ""){
+          
+        return res.json({
+            error: 'missing _id'
+          })
+
         } else {
 
-          Issue.findById(id,(err,data)=>{
+          if(Object.keys(updateFields).length>0){
 
-            if(err){
-              return res.json({
-              "error": err
-              })
-            }
+            Issue.findById(_id,(err,dataIssue)=>{
 
-            data.project = project;
-            data.issue_title = issue_title;
-            data.issue_text = issue_text; 
-            data.created_on = created_on; 
-            data.updated_on = updated_on; 
-            data.created_by = created_by; 
-            data.assigned_to = assigned_to; 
-            data.open = open; 
-            data.status_text = status_text;
+            if(err || dataIssue === null){
+              return res.json({ 
+                  error: 'could not update', 
+                  '_id': _id })
+            } else 
 
-            data.save((err,issue)=>{
-            if(err){
-              return res.json({
-              "error": err
-              })
-            }
-          
-              return res.json({
-                "_id": issue._id,
-                "issue_title": issue.issue_title,
-                "issue_text": issue.issue_text,
-                "created_on": issue.created_on,
-                "updated_on": issue.updated_on,
-                "created_by": issue.created_by,
-                "assigned_to": issue.assigned_to,
-                "open": issue.open,
-                "status_text": issue.status_text
+            if(dataIssue != null){
+
+              if(updateFields.issue_title){
+                dataIssue.issue_title = updateFields.issue_title;
+
+              }
+
+              if(updateFields.issue_text){
+                dataIssue.issue_text = updateFields.issue_text; 
+
+              }
+
+              if(updateFields.created_by){
+                dataIssue.created_by = updateFields.created_by; 
+
+              }
+
+              if(updateFields.assigned_to){
+                dataIssue.assigned_to = updateFields.assigned_to; 
+              }
+
+              if(updateFields.open){
+                dataIssue.open = updateFields.open;
+
+              }
+
+              if(updateFields.status_text){
+                dataIssue.status_text = updateFields.status_text;
+
+              }
+
+              dataIssue.updated_on = new Date().toISOString();
+
+               
               
-            })
+              dataIssue.save((err,issue)=>{
+              if(err || issue === null){
+                return res.json({ 
+                  error: 'could not update', 
+                  '_id': _id })
+              } else {
           
-          })
-        
-        })
+                return res.json({  result: 'successfully updated', '_id': _id })
 
-          
+              }
+
+              
+
+              })
+            }
+            
+            })
+          } else {
+            return res.json({ 
+            error: 'no update field(s) sent', 
+            '_id': _id })
+
+          }
+         
 
       }
         
@@ -237,35 +338,31 @@ module.exports = function (app) {
     })
     
     .delete(function (req, res){
+
       let project = req.params.project;
-      let id = req.body._id;
+      let _id = req.body._id;
+
 
       if(project){
-        if(id){
-          Issue.findByIdAndRemove(id, (err,issue)=>{
-            if(err){
-              return res.json({
-              "error": err
-              })
-            }
-
-            return res.json({
-                "_id": issue._id,
-                "issue_title": issue.issue_title,
-                "issue_text": issue.issue_text,
-                "created_on": issue.created_on,
-                "updated_on": issue.updated_on,
-                "created_by": issue.created_by,
-                "assigned_to": issue.assigned_to,
-                "open": issue.open,
-                "status_text": issue.status_text
-              
-            })
-          })
-
+        if(!_id){
+          return res.json({ error: 'missing _id' })
 
         } else{
-          return res.send('Please enter id')
+
+          Issue.findByIdAndRemove(_id, (err,issue)=>{
+            if(err || issue === null){
+              return res.json({ 
+                error: 'could not delete', 
+                '_id': _id })
+            } else {
+              return res.json({ 
+              result: 'successfully deleted', 
+              '_id': _id })
+
+            }
+
+            
+          })
 
         }
 
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,295 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  var firstId, secondId;
+  
+  suite("Create Tests", function () {
+    //Create an issue with every field: POST request to /api/issues/{project}
+    test("Create an issue with every field: POST request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .post("/api/issues/api-project")
+        .send({
+        issue_title: "First Issue Title", 
+        issue_text: "First Issue Text",  
+        created_by: "First Created By", 
+        assigned_to: "First Assigned To",
+        status_text: "First Status Text"
+        })
+        .end(function (err, res) {
+
+          firstId = res.body._id;
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, 'First Issue Title');
+          assert.equal(res.body.issue_text, 'First Issue Text');
+          assert.equal(res.body.created_by, 'First Created By');
+          assert.equal(res.body.assigned_to, 'First Assigned To');
+          assert.equal(res.body.open, true);
+          assert.equal(res.body.status_text, 'First Status Text');
+
+
+          done();
+        });
+    });
+
+    //Create an issue with only required fields: POST request to /api/issues/api-project
+    test("Create an issue with only required fields: POST request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .post("/api/issues/api-project")
+        .send({
+          issue_title: "Second Issue Title", 
+          issue_text: "Second Issue Text",  
+          created_by: "Second Created By"
+        })
+        .end(function (err, res) {
+          secondId = res.body._id;
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, 'Second Issue Title');
+          assert.equal(res.body.issue_text, 'Second Issue Text');
+          assert.equal(res.body.created_by, 'Second Created By');
+          assert.equal(res.body.open, true);
+
+
+          done();
+        });
+    });
+
+    //Create an issue with missing required fields: POST request to /api/issues/api-project
+    test("Create an issue with missing required fields: POST request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .post("/api/issues/api-project")
+        .send({
+
+        })
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, 'required field(s) missing');
+
+
+          done();
+        });
+    });
+
+    
+
+
+
+
+
+  })
+  
+  suite("View Tests", function () {
+    
+    //View issues on a project: GET request to /api/issues/api-project
+    test("View issues on a project: GET request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .get("/api/issues/api-project")
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.isArray(res.text.split(","));
+
+          done();
+        });
+    });
+    //View issues on a project with one filter: GET request to /api/issues/api-project
+    test("View issues on a project with one filter: GET request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .get("/api/issues/api-project?open=true")
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.isArray(JSON.parse(res.text));
+
+
+          done();
+        });
+    });
+
+    //View issues on a project with multiple filters: GET request to /api/issues/api-project
+    test("View issues on a project with multiple filters: GET request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .get("/api/issues/api-project?open=true&assigned_to=First Assigned To")
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.isArray(JSON.parse(res.text));
+
+
+          done();
+        });
+    });
+  })
+  
+  suite("Update Tests", function () {
+    //Update one field on an issue: PUT request to /api/issues/api-project
+    test("Update one field on an issue: PUT request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .put("/api/issues/api-project")
+        .send({
+          _id: firstId,
+          issue_title: "New First Issue Title"
+        })
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.result,'successfully updated')
+          assert.equal(res.body._id, firstId)
+
+
+          done();
+        });
+    });
+    //Update multiple fields on an issue: PUT request to /api/issues/api-project
+    test("Update multiple fields on an issue: PUT request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .put("/api/issues/api-project")
+        .send({
+          _id: firstId,
+          issue_text: "New First Issue Text",
+          created_by: "New First Created By",
+          assigned_to: "New First Assigned To"
+        })
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.result,'successfully updated')
+          assert.equal(res.body._id, firstId)
+
+
+          done();
+        });
+    });
+    //Update an issue with missing _id: PUT request to /api/issues/api-project
+    test("Update an issue with missing _id: PUT request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .put("/api/issues/api-project")
+        .send({
+        })
+        .end(function (err, res) {
+          
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.error,'missing _id')
+
+
+          done();
+        });
+    });
+    //Update an issue with no fields to update: PUT request to /api/issues/api-project
+    test("Update an issue with no fields to update: PUT request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .put("/api/issues/api-project")
+        .send({
+          _id: firstId
+        })
+        .end(function (err, res) {
+
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.error,'no update field(s) sent')
+          assert.equal(res.body._id,firstId)
+
+
+          done();
+        });
+    });
+    //Update an issue with an invalid _id: PUT request to /api/issues/api-project
+    test("Update an issue with an invalid _id: PUT request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .put("/api/issues/api-project")
+        .send({
+          _id: firstId +""+secondId,
+          issue_text: "New First Issue Text with Invalid Id",
+        })
+        .end(function (err, res) {
+                    
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.error,'could not update')
+          assert.equal(res.body._id,firstId +""+secondId)
+
+
+          done();
+        });
+    });
+
+    })
+  
+  suite("Delete Tests", function () {
+    //Delete an issue: DELETE request to /api/issues/api-project
+    test("Delete an issue: DELETE request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .delete("/api/issues/api-project")
+        .send({
+          _id: secondId
+        })
+        .end(function (err, res) {
+                    
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.result,'successfully deleted')
+          assert.equal(res.body._id,secondId)
+
+
+          done();
+        });
+    });
+    //Delete an issue with an invalid _id: DELETE request to /api/issues/api-project
+    test("Delete an issue with an invalid _id: DELETE request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .delete("/api/issues/api-project")
+        .send({
+          _id: firstId+""+secondId,
+        })
+        .end(function (err, res) {
+                    
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.error,'could not delete')
+          assert.equal(res.body._id,firstId+""+secondId)
+
+
+          done();
+        });
+    });
+    //Delete an issue with missing _id: DELETE request to /api/issues/api-project
+    test("Delete an issue with missing _id: DELETE request to /api/issues/api-project", function (done) {
+      chai
+        .request(server)
+        .delete("/api/issues/api-project")
+        .send({
+        })
+        .end(function (err, res) {
+                    
+          assert.equal(res.status, 200);
+          assert.equal(res.type,'application/json');
+          assert.equal(res.body.error,'missing _id')
+
+
+          done();
+        });
+    });
+    
+    })
+
+
   
 });
